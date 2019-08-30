87470e58f54d3091fda0f4e8cbb0774e54822843
Thiago Nascimento <thiago.nasciment8512@gmail.com>
2019-08-30 01:21:59 -0300

    Store e Middlewares configurados

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
 
 import '~/config/ReactotronConfig';
-
-import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 import history from './services/history';
 
+import { store, persistor } from './store';
+
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
