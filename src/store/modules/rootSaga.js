721ec5d5438e5b8153536811a8e3c1e93c3c1af9
Thiago Nascimento <thiago.nasciment8512@gmail.com>
2019-09-25 12:20:07 -0300

    Ajustado saga

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -5,5 +5,5 @@ import user from './user/sagas';
 import meetapp from './meetapp/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user, meetapp]);
+  yield all([auth, user, meetapp]);
 }
